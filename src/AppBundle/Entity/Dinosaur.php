98aac79f952b469f15a97141e4ae18fa200bdcaa
Habibun Noby <habibun.noby@gmail.com>
2023-03-04 09:19:45 +0600

    Chapter 04: TDD in Practice

--- src/AppBundle/Entity/Dinosaur.php
+++ src/AppBundle/Entity/Dinosaur.php
@@ -13,18 +13,27 @@ class Dinosaur
     public $length = 0;
 
     /**
-     * @return int
+     * @var string
+     * @ORM\Column(type="string")
      */
+    private $genus;
+    /**
+     * @var bool
+     * @ORM\Column(type="boolean")
+     */
+    private $isCarnivorous;
+
+    public function __construct(string $genus = 'Unknown', bool $isCarnivorous = false)
+    {
+        $this->genus = $genus;
+        $this->isCarnivorous = $isCarnivorous;
+    }
+
     public function getLength(): int
     {
         return $this->length;
     }
 
-    /**
-     * @param int $length
-     *
-     * @return Dinosaur
-     */
     public function setLength(int $length): Dinosaur
     {
         $this->length = $length;
@@ -32,4 +41,13 @@ class Dinosaur
         return $this;
     }
 
+    public function getSpecification(): string
+    {
+        return sprintf(
+            'The %s %scarnivorous dinosaur is %d meters long',
+            $this->genus,
+            $this->isCarnivorous ? '' : 'non-',
+            $this->length
+        );
+    }
 }
