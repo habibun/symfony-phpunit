27fec80c799c4ad0b13562c5b1b46148508fc88e
Habibun Noby <habibun.noby@gmail.com>
2023-03-10 14:44:05 +0600

    Chapter 27: Testing a Form Submit

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -37,4 +37,28 @@ class DefaultControllerTest extends WebTestCase
         $selector = sprintf('#enclosure-%s .button-alarm', $enclosure->getId());
         $this->assertGreaterThan(0, $crawler->filter($selector)->count());
     }
+
+    public function testItGrowsADinosaurFromSpecification()
+    {
+        $this->loadFixtures([
+            LoadBasicParkData::class,
+            LoadSecurityData::class,
+        ]);
+
+        $client = $this->makeClient();
+        $client->followRedirects();
+
+        $crawler = $client->request('GET', '/');
+        $this->assertStatusCode(200, $client);
+
+        $form = $crawler->selectButton('Grow dinosaur')->form();
+        $form['enclosure']->select(3);
+        $form['specification']->setValue('large herbivore');
+        $client->submit($form);
+
+        $this->assertContains(
+            'Grew a large herbivore in enclosure #3',
+            $client->getResponse()->getContent()
+        );
+    }
 }
